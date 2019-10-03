ef8901b7973c16a3e7c60eb6944997e5b42fa5fd
andriy <bileychyc@gmail.com>
2019-10-03 18:52:55 +0300

    Pershi failu kyrsy

---
+++ for.py
@@ -0,0 +1,11 @@
+for i in range(8):
+    for k in range (i+1):
+        print (f"k={k}, i={i}")
+    # print()
+    #if i % 2 == 0 : # парне
+        #for item in range(4):
+            #print("**  ", end="" )
+    #else:
+        #for item in range(4):
+            #print("  **", end="" )
+    #print()
